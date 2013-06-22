a56477a6427b0003c833222e76d8f4e5f7b9e1c2
Scott Walsh <scott@invisiblethreat.ca>
2013-06-22 15:08:14 -0300

    Update index.php
    You probably don't want my tracking code.

--- index.php
+++ index.php
@@ -52,20 +52,6 @@ a:link {color:#FFFFFF;text-decoration:none}
 a:hover {color:#F10000;text-decoration:none}
 a:visited {color:#F10000;text-decoration:none}
 </style>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-33788997-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
     <script type='text/javascript' src='https://www.google.com/jsapi'></script>
     <script type='text/javascript'>
      google.load('visualization', '1', {'packages': ['geochart']});
